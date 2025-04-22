14cf9d55ac41f8901b39aa03c1e38db5bf79c1e2
Sofiane <sofiane.bek@hotmail.com>
2025-04-22 22:59:08 +0200

    Complete all backend endpoints

--- backend/src/Controllers/taskController.ts
+++ backend/src/Controllers/taskController.ts
@@ -35,7 +35,7 @@ export async function getTaskById(req: Request, res: Response) {
         if (error instanceof Error){
             if (error.message === `cannot find task with id ${req.params.id}`){
                 res.status(404).send(error.message)
-            } else if (error.message === 'nvalid task id'){
+            } else if (error.message === 'Invalid task id'){
                 res.status(400).send(error.message)
             }
             else {
@@ -55,7 +55,7 @@ export async function createTask(req: Request, res: Response) {
     try {
         const newTask = new todoModel({title: task.title, 
             description: task.description, 
-            completed: task.completed ?? true})
+            completed: task.completed ?? false})
 
         newTask.save()
         res.status(201).send('Task created !')
@@ -70,10 +70,70 @@ export async function createTask(req: Request, res: Response) {
 }
 
 export async function updateTask(req: Request, res: Response) {
-    //TODO
+    
+    const {title, description, completed} = req.body
+
+    try {
+        if (!mongoose.isValidObjectId(req.params.id)){
+            throw new Error('Invalid task id')
+        }
+
+        const task: task | null= await todoModel.findById({_id: req.params.id})
+
+        if (!task){
+            throw new Error(`cannot find task with id ${req.params.id}`)
+        }
+
+        await todoModel.updateOne({ _id: req.params.id}, // Filter
+        { $set: { title: title ?? task.title, description: description ?? task.description, completed: completed ?? task.completed } })
+
+        res.status(200).send('Task Updated')
+
+    } catch(error){
+        if (error instanceof Error){
+            if (error.message === "Invalid task id"){
+                res.status(400).send("Bad request: Invalid Id format")
+            }
+            else if (error.message === `cannot find task with id ${req.params.id}`){
+                res.status(404).send(`cannot find task with id ${req.params.id}`)
+            }
+
+            res.status(500).send(error.message)
+        }
+        else {
+            res.status(500).send("An unknown error occured")
+        }
+    }
 }
 
 export async function deleteTask(req: Request, res: Response) {
-    //TODO
+    
+    try {
+        if (!mongoose.isValidObjectId(req.params.id)){
+            throw new Error('Invalid task id')
+        }
+   
+        const id = req.params.id
+        const task: task | null = await todoModel.findOneAndDelete({_id: id})
+        if (!task){
+            throw new Error(`cannot find task with id ${req.params.id}`)
+        }
+        res.status(200).send("Task successfully deleted !")
+   
+    } catch (error) {
+        if (error instanceof Error){
+            if (error.message === "Invalid task id"){
+                res.status(400).send("Bad request: Invalid Id format")
+            }
+            else if (error.message === `cannot find task with id ${req.params.id}`){
+                res.status(404).send(`cannot find task with id ${req.params.id}`)
+            }
+
+            res.status(500).send(error.message)
+        }
+        else {
+            res.status(500).send("An unknown error occured")
+        }
+    }
 }
 
